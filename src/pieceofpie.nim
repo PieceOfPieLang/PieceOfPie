2de66882cbbaab1ab446018d8b9f8aab1dc7ab5f
EagleOnGitHub <imnuex@gmail.com>
2020-12-02 15:13:32 +0200

    Whoops... forgot to commit... Added benchmarking

--- src/pieceofpie.nim
+++ src/pieceofpie.nim
@@ -13,10 +13,15 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ]#
+import times, os, strutils, strformat
 
-import os
-import strutils
-import strformat
+template benchmark(benchmarkName: string, code: untyped) =
+  block:
+    let t0 = epochTime()
+    code
+    let elapsed = epochTime() - t0
+    let elapsedStr = elapsed.formatFloat(format = ffDecimal, precision = 3)
+    echo "CPU Time for [", benchmarkName, "]: ", elapsedStr, "s"
 
 let args = commandLineParams()
 let fileToCompile = args[0]
@@ -24,31 +29,33 @@ let fileSplit = splitFile(fileToCompile)
 let outputBinaryFile = fileSplit.name
 var outputC = open(outputBinaryFile & ".c", fmWrite)
 echo "Transpiling to C...."
-outputC.writeLine(r"#include <stdio.h>")
-outputC.writeLine(r"")
-outputC.writeLine(r"int main() {")
-for line in fileToCompile.lines:
-    var a = cast[string](line).split(' ')
-    var reala = a
-    a.delete(0)
-    var atostring = a.join(" ")
-    if atostring.len >= 5:
-      if cmpIgnoreCase(atostring[0..4], "!num ") == 0:
-        atostring.delete(0, 4)
-    for i, c in reala:
-        if cmpIgnoreCase(c, "print") == 0:
-            outputC.write("\tprintf(")
-            for i, c in a:
-                if cmpIgnoreCase(c, "!num") == 0:
-                  outputC.write("\"%d\", ")
-            outputC.write(atostring)
-            outputC.writeLine(");")
-outputC.write("}")
-outputC.close()
+benchmark "transpiling":
+  outputC.writeLine(r"#include <stdio.h>")
+  outputC.writeLine(r"")
+  outputC.writeLine(r"int main() {")
+  for line in fileToCompile.lines:
+      var a = cast[string](line).split(' ')
+      var reala = a
+      a.delete(0)
+      var atostring = a.join(" ")
+      if atostring.len >= 5:
+        if cmpIgnoreCase(atostring[0..4], "!num ") == 0:
+          atostring.delete(0, 4)
+      for i, c in reala:
+          if cmpIgnoreCase(c, "print") == 0:
+              outputC.write("\tprintf(")
+              for i, c in a:
+                  if cmpIgnoreCase(c, "!num") == 0:
+                    outputC.write("\"%d\", ")
+              outputC.write(atostring)
+              outputC.writeLine(");")
+  outputC.write("}")
+  outputC.close()
 echo "Done!\n"
 echo "Compiling..."
 let compileCmd = "gcc " & outputBinaryFile & ".c" & " -o " & outputBinaryFile & ".exe"
 echo fmt"Compile command: {compileCmd}"
 echo ""
-discard execShellCmd(compileCmd)
+benchmark "compiling":
+  discard execShellCmd(compileCmd)
 removeFile(outputBinaryFile & ".c")
